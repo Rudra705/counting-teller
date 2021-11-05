01aa5b800cdbfc43aa2bf7db642067931c8239c7
Rudra705 <rudrarathi620@gmail.com>
2021-11-05 11:57:37 +0530

    Author -> Rudra Rathi

---
+++ counting-teller.py
@@ -0,0 +1,11 @@
+i = 1
+l = int(input("Enter the nmber till you want the counting...\n"))
+print("Starting!")
+import time
+for i in range(l):
+	i = i + 1
+	print(i)
+	time.sleep(0.5) 
+
+print("Done!")
+time.sleep(2)
